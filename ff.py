37878db76451efa608c4c7a9c07d6cf3a639e983
Raj Gupta <raazgupta@gmail.com>
2011-09-05 12:54:29 +0800

    added more functions

--- ff.py
+++ ff.py
@@ -12,10 +12,80 @@ def findRegex(findStr, regexStr):
 def fairnessFunction(fairStr, idNum, fairNum):
     fairRegex = r'.*[Ff][Aa][Ii][Rr].*'
     isFair = findRegex(fairStr, fairRegex)
-    if isFair == True:
+    goodLookingRegex = r'.*[gG][oO][oO][dD].*[lL][oO][oO][kK][iI][nN][gG].*'
+    isGoodLooking = findRegex(fairStr, goodLookingRegex)
+    beautifulRegex = r'.*[bB][eE][aA][uU][tT][iI][fF][uU][lL].*'
+    isBeautiful = findRegex(fairStr, beautifulRegex)
+    if isFair == True or isGoodLooking == True or isBeautiful == True:
         fairNum = fairNum + 1
-        print str(idNum) + " needs Fair:" + oddStr
+        #print str(idNum) + " needs Fair:" + fairStr
     return fairNum
+
+def intelligenceFunction(inStr, idNum, inNum):
+    intelligenceRegex = r'.*[iI][nN][tT][eE][lL][lL][iI][gG][eE][nN][tT].*'
+    isIntelligent = findRegex(inStr, intelligenceRegex)
+    smartRegex = r'.*[sS][mM][aA][rR][tT].*'
+    isSmart = findRegex(inStr, smartRegex)
+    educatedRegex = r'.*[eE][dD][uU][cC][aA][tT][eE][dD].*'
+    isEducated = findRegex(inStr, educatedRegex)
+    if isIntelligent == True or isSmart == True or isEducated == True:
+        inNum = inNum + 1
+        #print str(idNum) + " needs Intelligent:" + inStr
+    return inNum
+
+def girlIntelligenceFunction(ginStr, idNum, ginNum):
+    adFound = False
+    
+    #Synonyms for intelligence
+    intelligenceRegexArray = [r'.*[eE]ducated.*', r'.*[iI]ntelligent.*', r'.*[sS]mart.*']
+
+    #Remove the cases where an intelligent boy is being advertised
+    intelligentBoyRegexArray = [r'.*[Bb]oy.*', r'.*[Gg]room.*', r'.*[Gg]uy.*', r'.*[Pp]arents.*', r'.*[Ff]amily.*', r'.*[Dd]ivorcee.*']
+    
+    #Synonyms for bride    
+    brideRegexArray = [r'[mM]atch.*', r'[bB]ride.*', r'[gG]irl.*']
+                
+    #Controlling for ads that advertise the boy as intelligent
+    #but do not care about the girl
+    
+    resultWithoutBoy = False
+    for inRegex in intelligenceRegexArray:
+        for brideRegex in brideRegexArray:
+            withoutBoyCombiRegex = inRegex + brideRegex
+            iswithoutBoyBride = findRegex(ginStr, withoutBoyCombiRegex)
+            if iswithoutBoyBride == True:
+                resultWithoutBoy = True
+    
+    resultWithBoy = False
+    for inRegex in intelligenceRegexArray:
+        for brideRegex in brideRegexArray:
+            for boyRegex in intelligentBoyRegexArray:
+                withBoyCombiRegex = inRegex + boyRegex + brideRegex
+                iswithBoyBride = findRegex(ginStr, withBoyCombiRegex)
+                if iswithBoyBride == True:
+                    resultWithBoy = True
+    
+    if resultWithoutBoy == True and resultWithBoy == False:
+        adFound = True
+    else:
+        resultWithBG = False
+        for inRegex in intelligenceRegexArray:
+            for brideRegex in brideRegexArray:
+                for boyRegex in intelligentBoyRegexArray:
+                    withBoywithGirlRegex = inRegex + boyRegex + inRegex + brideRegex
+                    isbg = findRegex(ginStr, withBoywithGirlRegex)
+                    if isbg == True:
+                        resultWithBG = True
+        
+        if resultWithBG == True:
+            adFound = True
+                    
+    if adFound == True:
+        ginNum = ginNum + 1
+        print str(idNum) + " needs Intelligent Bride:" + ginStr
+    
+    return ginNum
+    
 
 #Loop through 10 marriage classified pages
 
@@ -23,6 +93,11 @@ def fairnessFunction(fairStr, idNum, fairNum):
 totalAds = 1.0
 #number of fair requests
 fairNum = 0.0
+#number of intelligent requests
+inNum = 0.0
+#number of intelligent girl requests
+ginNum = 0.0
+
 for pageNum in range(1,11):
     print "Processing page " + str(pageNum) + " ..."
 
@@ -46,14 +121,21 @@ for pageNum in range(1,11):
         #process the string for interesting patterns
         #find strings with the word fair in it
         fairNum = fairnessFunction(oddStr, totalAds, fairNum)
-          
+        inNum = intelligenceFunction(oddStr, totalAds, inNum)
+        ginNum = girlIntelligenceFunction(oddStr, totalAds, ginNum)
         #Even numbered classified information
         evenStr = classified.nextSibling.string 
         #find strings with the word fair in it
         fairNum = fairnessFunction(evenStr, totalAds, fairNum)
+        inNum = intelligenceFunction(evenStr, totalAds, inNum)
+        ginNum = girlIntelligenceFunction(evenStr, totalAds, ginNum)
         
         totalAds = totalAds + 2
     
 #Print a percentage of number asking for Fair vs total Ads
 fairPer = fairNum / totalAds * 100.0
-print "Percentage asking for a Fair better half: " + str(fairPer)
+print "Percentage valuing Fairness: " + str(fairPer)
+inPer = inNum / totalAds * 100.0
+print "Percentage valuing Intelligence: " + str(inPer)
+ginPer = ginNum / totalAds * 100.0
+print "Percentage valuing Intelligent brides: " + str(ginPer)
